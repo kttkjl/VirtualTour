a9eb73893f43dd39b536091f04a7b27959b7869c
Simon Wu <swu111@my.bcit.ca>
2017-03-03 11:38:17 -0800

    javascript to form added

--- Simon (1)/Forms/javaform.js
+++ Simon (1)/Forms/javaform.js
@@ -1,16 +1,19 @@
 function form_validate(){
 	var formValue = document.getElementById("form_TextArea").value;
+	var formSpecial = new RegExp(/[&'"\$\\]/g);
 	if(formValue == ""){
 		document.getElementById("form_TextArea").placeholder = "Please enter a comment";
 		return false;
+	} else if(formSpecial.test(formValue)){
+			formValue=formValue.replace(/&/g, "&amp;");
+			formValue=formValue.replace(/'/g, "&#39;");
+			formValue=formValue.replace(/"/g, "&quot;");
+			formValue=formValue.replace(/\$/g, "&#36;");
+			formValue=formValue.replace(/\\/g, "&#92;");
+			alert(formValue);
+			return false;
 	} else {
-		formValue.replace("\"", "&#39;");
-		formValue.replace("\"", "&quot;");
-		formValue.replace("$", "&#36;");
-		formValue.replace("&", "&amp;");
-		formValue.replace("\\", "&#92;");
-		formValue.replace("/d/g", "f");
-		alert(formValue);
-		return false;
+			alert("correct");
+			return true;
 	}
 }
