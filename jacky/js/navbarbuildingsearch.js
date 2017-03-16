b8c2b12976057bc9a2f8e2f7c7e54ea2cd637fd7
kttkjl <jinglun.li@alumni.ubc.ca>
2017-03-15 19:15:55 -0700

    added the map to be used? - see my folder for the .psd

---
+++ jacky/js/navbarbuildingsearch.js
@@ -0,0 +1,14 @@
+
+function sendToPage(id){
+    var input = document.getElementById(id).value;
+    var correctedInput = input.toLowerCase();
+     //alert(input);
+    switch (correctedInput) {
+        case "sw1":
+            location.href = "/buildings/sw1.html";
+            break;
+        case "sw2":
+            location.href = "/buildings/sw1.html";
+            break;
+
+}
