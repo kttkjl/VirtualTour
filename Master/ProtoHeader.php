65349a16dc906031b07650286e93ab420ed363fd
aharaguchi <akharaguchi@gmail.com>
2017-04-08 02:06:45 -0700

    Added media queries, updated proto/phps with appropriate updated data, changed anchor tags from html to php for our pages

--- Master/ProtoHeader.php
+++ Master/ProtoHeader.php
@@ -2,6 +2,7 @@
 	<html lang="en">
 		<head>
 			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<link rel="stylesheet" href="css/theme.css">
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 			<script>
--- Master/ProtoNavLogged.php
+++ Master/ProtoNavLogged.php
@@ -1,29 +1,71 @@
-
 </head>
-	<body>
-			<nav>
-			<h1><a href="index.html">Virtual Tour</a></h1>
-			<ul>
-				<div id="acc"><li><a href="account.html">My Account</a></li></div>
-				<div id="conus"><li><a href="contactus.html">Contact Us</a></li></div>
-				<div id="gloss"><li><a href="glossary.html">Glossary</a></li></div>
-				<div id="abuild"><li class="dropdown"><a href="allbuild.html" class="dropbtn">Buildings</a>
-				<div class="dropdown-content"> <!--Div for the part that actually drops down. -->
-					<a href="eachbuild.html">Link 1 </a>
-					<a href="eachbuild.html">Link 2 </a>
-					<a href="eachbuild.html">Link 3 </a>
-					<a href="eachbuild.html">Link 4 </a>
-					<a href="eachbuild.html">Link 5 </a>
-				</div> <!-- End of Div "dropdown-content"-->
-				</li></div>
-				<div id="hom"><li><a href="index.html">Home</a></li></div>
-			</ul>
-		</nav>
-<main>
-	<div id ="bannerPic"> <!--Div for the banner image.-->
-	<img src="images/panorama2.jpg" alt="British Columbia" width="1752" height="500" id="bannerBG">
-	</div> <!-- end of bannerPic div-->
-	<div class="userControl userControlColor"> <!-- Div for the second nav bar (the one with user controls -->
+			<body>
+					<div id="theEntireNav">
+					<nav>
+					<h1><a href="index.php">Virtual Tour</a></h1>
+					<div id="navbarBox">
+					<ul id="main-nav-list">
+						<div id="acc"><li class="main-list-link"><a href="account.php">My Account</a></li></div>
+						<div id="conus"><li class="main-list-link"><a href="contactus.php">Contact Us</a></li></div>
+						<div id="gloss"><li class="main-list-link"><a href="glossary.php">Glossary</a></li></div>
+						<div id="abuild"><li class="dropdown main-list-link"><a href="allbuild.php" class="dropbtn">Buildings</a>
+						<div class="dropdown-content"> <!--Div for the part that actually drops down. -->
+			<div id="navcontainer">
+			<table id="navtable" title="The drop-down navigation">
+				<tr class="navtablerow">
+					<td class="navtableheading"><a href="northeast.php">Northeast</a></td>
+					<td class="navtableheading"><a href="northwest.php">NorthWest</a></td>
+					<td class="navtableheading"><a href="southeast.php">SouthEast</a></td>
+					<td class="navtableheading"><a href="southwest.php">SouthWest</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td class="navtabledata"><a href =".php">EE 1</a></td>
+					<td class="navtabledata"><a href =".php">NE 2</a></td>
+					<td class="navtabledata"><a href =".php">NE 3</a></td>
+					<td class="navtabledata"><a href =".php">NE 4</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td class="navtabledata"><a href =".php">EE 1</a></td>
+					<td class="navtabledata"><a href =".php">NE 2</a></td>
+					<td class="navtabledata"><a href =".php">NE 3</a></td>
+					<td class="navtabledata"><a href =".php">NE 4</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td class="navtabledata"><a href =".php">EE 1</a></td>
+					<td class="navtabledata"><a href =".php">NE 2</a></td>
+					<td class="navtabledata"><a href =".php">NE 3</a></td>
+					<td class="navtabledata"><a href =".php">NE 4</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td class="navtabledata"><a href =".php">EE 1</a></td>
+					<td class="navtabledata"><a href =".php">NE 2</a></td>
+					<td class="navtabledata"><a href =".php">NE 3</a></td>
+					<td class="navtabledata"><a href =".php">NE 4</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td class="navtabledata"><a href =".php">EE 1</a></td>
+					<td class="navtabledata"><a href =".php">NE 2</a></td>
+					<td class="navtabledata"><a href =".php">NE 3</a></td>
+					<td class="navtabledata"><a href =".php">NE 4</a></td>
+				</tr>
+				<tr class="navtablerow">
+					<td colspan="4" class="navtablebottom">...Or for more, go to the <a href="allbuild.php">buildings page.</a></td>
+				</tr>
+			</table>
+			</div>
+						</div> <!-- End of Div "dropdown-content"-->
+						</li>
+						</div>
+						<div id="home"><li class="main-list-link"><a href="index.php">Home</a></li></div>
+					</ul>
+					</div>
+				</nav>
+				</div>
+		<main>
+			<div id ="bannerPic"> <!--Div for the banner image.-->
+			<img src="images/panorama2.jpg" alt="British Columbia" width="1577" height="450" id="bannerBG">
+			</div> <!-- end of bannerPic div-->
+			<div class="userControl"> <!-- Div for the second nav bar (the one with user controls -->
 	<?php
 		require_once('PDO_conn.php');
 	
@@ -41,8 +83,8 @@
 			echo '
 			<h2>Currently Not logged in </h2>
 			<ul>
-			<li><a href="login.html">Sign in</a></li>
-			<li><a href="registration.html">Register</a></li>
+			<li><a href="login.php">Sign in</a></li>
+			<li><a href="registration.php">Register</a></li>
 			';
 		}	
 	?>	
