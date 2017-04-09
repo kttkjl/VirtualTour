0efa1f9d276bfd387b3613b2fef89e20b39d53f3
aharaguchi <akharaguchi@gmail.com>
2017-04-09 16:41:32 -0700

    Most updated index codes are in PROTOphp file as then i only need to change it in one place

--- Master/ProtoFooter.php
+++ Master/ProtoFooter.php
@@ -1,144 +1,99 @@
-	</main>
-	<footer>
-	<div class="footer_wrapper">
-		<div class="footer_table">
-			<table class="table_outer">
-				<tr> 
-					<th> NE
-						<table class="table_col">
-							<tr>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-						</table>
-					</th>
-					<th> NW
-						<table class="table_col">
-							<tr>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-						</table>
-					</th>
-					<th> SE
-						<table class="table_col">
-							<tr>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-						</table>
-					</th>
-					<th> SW
-						<table class="table_col">
-							<tr>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-						</table>
-					</th>
-					<th> Others
-						<table class="table_col">
-							<tr>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-							<tr><td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td>
-								<td><a href ="index.html">blah</a></td></tr>
-						</table>
-					</th>
-				</tr>
-			</table>
-			<p class="footer_copy"> COPYRIGHT AND CONTACT US. || 2017</p>
-			<h2 class="footer_media"> FB twitter </h2>
+</main>
+		<footer>
+		<div class="footer_wrapper">
+			<div class="footer_table">
+				<table class="table_outer">
+					<tr> 
+						<th> NE
+							<table class="table_col">
+								<tr>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+							</table>
+						</th>
+						<th> NW
+							<table class="table_col">
+								<tr>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+							</table>
+						</th>
+						<th> SE
+							<table class="table_col">
+								<tr>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+							</table>
+						</th>
+						<th> SW
+							<table class="table_col">
+								<tr>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+							</table>
+						</th>
+						<th> Others
+							<table class="table_col">
+								<tr>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+								<tr><td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td>
+									<td><a href ="index.php">blah</a></td></tr>
+							</table>
+						</th>
+					</tr>
+				</table>
+				<p class="footer_copy"> COPYRIGHT AND CONTACT US. || 2017</p>
+				<h2 class="footer_media"> FB twitter </h2>
+			</div>
 		</div>
-	</div>
-</footer>
-</html>
+	</footer>
+	</html>
