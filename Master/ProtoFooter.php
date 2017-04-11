03e91f7abf63cd0e7a798cd5757c77389ac8aa8f
Akemi <akharaguchi@gmail.com>
2017-04-10 21:53:01 -0700

    Changed Footer, changed theme.css to reflect the changes.

--- Master/ProtoFooter.php
+++ Master/ProtoFooter.php
@@ -2,8 +2,9 @@
 		<footer>
 		<div class="footer_wrapper">
 			<div class="footer_table">
-				<p class="footer_copy"> COPYRIGHT AND CONTACT US. || 2017</p>
-				<h2 class="footer_media"> FB twitter </h2>
+				<p class="footer_copy"><a href="sitemap.php">Site Map</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="hope.php">Home</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="allbuild.php">Buildings</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="glossary.php">Glossary</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="contactus.php">Contact Us</a>
+				<br>&copy; COPYRIGHT 2017 Group 12. All Rights Reserved.</p>
+				<h2 class="footer_media"><a href="www.facebook.com">Facebook</a> &nbsp;||&nbsp; <a href="www.twitter.com">Twitter</a> </h2>
 			</div>
 		</div>
 	</footer>
