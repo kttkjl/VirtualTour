5ff2e1396bd7bdbffc6539d65dab1e0266ec7e6c
Simon Wu <swu111@my.bcit.ca>
2017-04-10 19:24:21 -0700

    Add protoheader and protofooter to contactus.php and allbuild.php

--- Master/contactus.php
+++ Master/contactus.php
@@ -1,101 +1,22 @@
 <?php
 require_once('PDO_conn.php');
-
-/*if($user->is_loggedin()){
-	$haveSignedIn = true;
-} else {
-	$haveSignedIn = false;
-}*/
-$haveSignedIn = $user->is_loggedin();
+include("ProtoHeader.php");
 ?>
+<!--Page-specific <title>-->
+<title>Virtual Tour - Contact Us</title>
+<!--Page-specific <js and css>-->
 
+<link rel="stylesheet" href="css/theme.css" media="screen">
+<link rel="stylesheet" href="css/contact.css" media="screen">
+<link rel="stylesheet" href="css/print.css" media="print">
+<script type="text/javascript" src="css/contact.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="css/mapWidget.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEI3ykIDITnjeg0qCAAzYBQzWALRffmGM&callback=initMap"></script>
 
-<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="utf-8"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="stylesheet" href="css/theme.css" media="screen">
-			<link rel="stylesheet" href="css/contact.css" media="screen">
-			<link rel="stylesheet" href="css/print.css" media="print">
-			<script type="text/javascript" src="css/contact.js"></script>
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-			<script src="css/mapWidget.js"></script>
-			<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEI3ykIDITnjeg0qCAAzYBQzWALRffmGM&callback=initMap"></script>
-			<title>Layout B</title>
-		</head>
-			<body>
-					<nav>
-					<h1><a href="index.php">Virtual Tour</a></h1>
-					<div id="navbarBox">
-					<ul id="main-nav-list">
-						<div id="conus"><li class="main-list-link"><a href="contactus.php">Contact Us</a></li></div>
-						<div id="gloss"><li class="main-list-link"><a href="glossary.php">Glossary</a></li></div>
-						<div id="abuild"><li class="dropdown main-list-link"><a href="allbuild.php" class="dropbtn">Buildings</a>
-						<div class="dropdown-content"> <!--Div for the part that actually drops down. -->
-			<div id="navcontainer">
-			<table id="navtable" title="The drop-down navigation">
-				<tr class="navtablerow">
-					<td class="navtableheading"><a href="northeast.php">Northeast</a></td>
-					<td class="navtableheading"><a href="northwest.php">NorthWest</a></td>
-					<td class="navtableheading"><a href="southeast.php">SouthEast</a></td>
-					<td class="navtableheading"><a href="southwest.php">SouthWest</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td colspan="4" class="navtablebottom">...Or for more, go to the <a href="allbuild.php">buildings page.</a></td>
-				</tr>
-			</table>
-			</div>
-						</div> <!-- End of Div "dropdown-content"-->
-						</li>
-						</div>
-						<div id="home"><li class="main-list-link"><a href="index.php">Home</a></li></div>
-					</ul>
-					</div>
-				</nav>
-		<main>
-			<div id ="bannerPic"> <!--Div for the banner image.-->
-			<img src="images/panorama2.jpg" alt="British Columbia" width="1752" height="300" id="bannerBG">
-			</div> <!-- end of bannerPic div-->
-			<div class="userControl"> <!-- Div for the second nav bar (the one with user controls -->
-			<h2>Logged in as : Group A</h2>
-			<ul>
-				<li> <a href="account.html">User controls</a></li>
-				<li id="In" class=""><a href="login.html">Sign in</a></li>
-				<li id="Out" class="signOut"><a href="startRun.php">Sign out</a></li>
-				<li> <a href="registration.html">Register</a></li>
-			</ul>
-			</div> <!--End of userControl -->
+<?php
+include("ProtoNavLogged.php");
+?>
 		<div id="mainContent">  <!-- Insert your part of the website here. -->
 			<div class="Body_Area">
 			<h2> Contact Us </h2>
@@ -293,15 +214,4 @@ $haveSignedIn = $user->is_loggedin();
 		</div>
 	</footer>
 	</html>
-	
-	<script>
-	var php_var = "<?php echo $haveSignedIn; ?>";
-	if(php_var){
-		document.getElementById("In").className = "signIn";
-		document.getElementById("Out").className = "";
-	} else {
-		alert("not signed in");
-		document.getElementById("In").className = "";
-		document.getElementById("Out").className = "signOut";
-	}
-</script>
+<?php include ("ProtoFooter.php"); ?>
