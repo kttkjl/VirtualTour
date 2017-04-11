c05299e6cdd7822f8fa6b1b9dcad23a6a6a4c7da
Ian Lo <ilo5@my.bcit.ca>
2017-04-11 09:57:34 -0700

    proto header, proto footer added to index.php

--- Master/index.php
+++ Master/index.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="utf-8"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="stylesheet" href="css/theme.css" media="screen">
-			<link rel="stylesheet" href="css/index.css" media="screen">
-			<link rel="stylesheet" href="css/print.css" media="print">
+<?php 
+require_once('PDO_conn.php');
+include("ProtoHeader.php"); 
+
+$haveSignedIn = $user->is_loggedin();
+
+$myConnection = $DB_conn;
+$myQuery = $myConnection->prepare(
+				"SELECT * 
+				FROM comments"
+				);
+$myQuery->execute();
+/*$result = $myQuery->setFetchMode(PDO::FETCH_ASSOC);*/
+$result = $myQuery;?>
 			<title>Virtual Tour - Home</title>
+			<link rel="stylesheet" href="css/index.css" media="screen">
 			<script language="JavaScript1.1">
 				var slideimages=new Array()
 				var slidelinks=new Array()
@@ -30,92 +37,7 @@
 					winslide.focus()
 				}
 			</script>
-			</head>
-			<body>
-					<div id="theEntireNav">
-					<nav style="position:static;">
-					<h1><a href="index.php">Virtual Tour</a></h1>
-					<div id="navbarBox">
-					<ul id="main-nav-list">
-						<div id="conus"><li class="main-list-link"><a href="contactus.php">Contact Us</a></li></div>
-						<div id="gloss"><li class="main-list-link"><a href="glossary.php">Glossary</a></li></div>
-						<div id="abuild"><li class="dropdown main-list-link"><a href="allbuild.php" class="dropbtn">Buildings</a>
-						<div class="dropdown-content"> <!--Div for the part that actually drops down. -->
-			<div id="navcontainer">
-			<table id="navtable" title="The drop-down navigation">
-				<tr class="navtablerow">
-					<td class="navtableheading"><a href="eachbuild.php">Northeast</a></td>
-					<td class="navtableheading"><a href="eachbuild.php">NorthWest</a></td>
-					<td class="navtableheading"><a href="eachbuild.php">SouthEast</a></td>
-					<td class="navtableheading"><a href="eachbuild.php">SouthWest</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td class="navtabledata"><a href =".php">EE 1</a></td>
-					<td class="navtabledata"><a href =".php">NE 2</a></td>
-					<td class="navtabledata"><a href =".php">NE 3</a></td>
-					<td class="navtabledata"><a href =".php">NE 4</a></td>
-				</tr>
-				<tr class="navtablerow">
-					<td colspan="4" class="navtablebottom">...Or for more, go to the <a href="allbuild.php">buildings page.</a></td>
-				</tr>
-			</table>
-			</div>
-						</div> <!-- End of Div "dropdown-content"-->
-						</li>
-						</div>
-						<div id="home"><li class="main-list-link"><a href="index.php">Home</a></li></div>
-					</ul>
-					</div>
-				</nav>
-				</div>
-		<div class="userControl"> <!-- Div for the second nav bar (the one with user controls -->
-	<?php
-        require_once('PDO_conn.php');
-
-
-        if(isset($_SESSION['username']))
-        {
-            echo 
-            '<h2>Logged in as : '.$_SESSION['username'].'</h2>'
-            .'<ul>
-            <li><a href="account.php">User controls</a></li>
-            <li><a href="startRun.php">Logout</a></li>
-            ';
-        } else {
-            echo '
-            <h2>Currently Not logged in </h2>
-            <ul>
-            <li><a href="login.php">Sign in</a></li>
-            <li><a href="registration.php">Register</a></li>
-            ';
-        }
-    ?>
-		</ul>
-	</div> <!--End of userControl -->
+			<?php include("ProtoNavLogged.php"); ?>
 	<main>
 		<img src="images/image0.jpg" name="slide" border=0 width=900 height=528>
 <script>
@@ -155,14 +77,4 @@ slideit()
 		</div>
 		<h3>To get started, go to our <a href="allbuild.php" class="linkUl"> buildings</a> page.</h3>
 		</div>
-		</main>
-		<footer>
-		<div class="footer_wrapper">
-			<div class="footer_table">
-				<p class="footer_copy"><a href="sitemap.php">Site Map</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="hope.php">Home</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="allbuild.php">Buildings</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="glossary.php">Glossary</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="contactus.php">Contact Us</a>
-				<br>&copy; COPYRIGHT 2017 Group 12. All Rights Reserved.</p>
-				<h2 class="footer_media"><a href="www.facebook.com">Facebook</a> &nbsp;||&nbsp; <a href="www.twitter.com">Twitter</a> </h2>
-			</div>
-		</div>
-	</footer>
-	</html>
+		<?php include ("ProtoFooter.php"); ?>
