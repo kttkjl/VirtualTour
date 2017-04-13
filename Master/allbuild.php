138f3434912ffce6e3f34fb6f9e51aec5d057d31
Ian Lo <ilo5@my.bcit.ca>
2017-04-13 13:49:03 -0700

    added missing buildings

--- Master/allbuild.php
+++ Master/allbuild.php
@@ -7,7 +7,6 @@ include("ProtoHeader.php");
 <title>Virtual Tour - Buildings Overview</title>
 <!--Page-specific <js and css>-->
 <link rel="stylesheet" href="css/allbuild.css">
-<link rel="stylesheet" href="css/print.css" media="print">
 
 <?php
 include("ProtoNavLogged.php");
@@ -25,10 +24,10 @@ include("ProtoNavLogged.php");
 			<table>
 				<tr><td colspan="8">Buildings in this sector:</td></tr>
 				<tr>
-					<td colspan="2"><a href="javascript:void(0);">NW 1</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NW 3</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NW 5</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NW 6</a></td>
+					<td colspan="2"><a href="eachbuildNW1.php">NW 1</a></td>
+					<td colspan="2"><a href="eachbuildNW3.php">NW 3</a></td>
+					<td colspan="2"><a href="eachbuildNW5.php">NW 5</a></td>
+					<td colspan="2"><a href="eachbuildNW6.php">NW 6</a></td>
 				</tr>
 			</table>
 		</div>
@@ -45,30 +44,30 @@ include("ProtoNavLogged.php");
 			<table>
 				<tr><td colspan="8">Buildings in this sector:</td></tr>
 				<tr>
-					<td><a href="javascript:void(0);">NE 1</a></td>
-					<td><a href="javascript:void(0);">NE 2</a></td>
-					<td><a href="javascript:void(0);">NE 3</a></td>
-					<td><a href="javascript:void(0);">NE 4</a></td>
-					<td><a href="javascript:void(0);">NE 5</a></td>
-					<td><a href="javascript:void(0);">NE 6</a></td>
-					<td><a href="javascript:void(0);">NE 7</a></td>
-					<td><a href="javascript:void(0);">NE 8</a></td>
+					<td><a href="eachbuildNE1.php">NE 1</a></td>
+					<td><a href="eachbuildNE2.php">NE 2</a></td>
+					<td><a href="eachbuildNE3.php">NE 3</a></td>
+					<td><a href="eachbuildNE4.php">NE 4</a></td>
+					<td><a href="eachbuildNE5.php">NE 5</a></td>
+					<td><a href="eachbuildNE6.php">NE 6</a></td>
+					<td><a href="eachbuildNE7.php">NE 7</a></td>
+					<td><a href="eachbuildNE8.php">NE 8</a></td>
 				</tr>
 				<tr>
-					<td><a href="javascript:void(0);">NE 9</a></td>
-					<td><a href="javascript:void(0);">NE 10</a></td>
-					<td><a href="javascript:void(0);">NE 12</a></td>
-					<td><a href="javascript:void(0);">NE 16</a></td>
-					<td><a href="javascript:void(0);">NE 18</a></td>
-					<td><a href="javascript:void(0);">NE 20</a></td>
-					<td><a href="javascript:void(0);">NE 21</a></td>
-					<td><a href="javascript:void(0);">NE 22</a></td>
+					<td><a href="eachbuildNE9.php">NE 9</a></td>
+					<td><a href="eachbuildNE10.php">NE 10</a></td>
+					<td><a href="eachbuildNE12.php">NE 12</a></td>
+					<td><a href="eachbuildNE16.php">NE 16</a></td>
+					<td><a href="eachbuildNE18.php">NE 18</a></td>
+					<td><a href="eachbuildNE20.php">NE 20</a></td>
+					<td><a href="eachbuildNE21.php">NE 21</a></td>
+					<td><a href="eachbuildNE22.php">NE 22</a></td>
 				</tr>
 				<tr>
-					<td colspan="2"><a href="javascript:void(0);">NE 23</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NE 24</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NE 25</a></td>
-					<td colspan="2"><a href="javascript:void(0);">NE 28</a></td>
+					<td colspan="2"><a href="eachbuildNE23.php">NE 23</a></td>
+					<td colspan="2"><a href="eachbuildNE24.php">NE 24</a></td>
+					<td colspan="2"><a href="eachbuildNE25.php">NE 25</a></td>
+					<td colspan="2"><a href="eachbuildNE28.php">NE 28</a></td>
 				</tr>
 			</table>
 		</div>
@@ -85,14 +84,12 @@ include("ProtoNavLogged.php");
 			<table>
 				<tr><td colspan="8">Buildings in this sector:</td></tr>
 				<tr>
-					<td><a href="javascript:void(0);"></a></td>
-					<td><a href="javascript:void(0);">SW 1</a></td>
-					<td><a href="javascript:void(0);">SW 2</a></td>
-					<td><a href="javascript:void(0);">SW 3</a></td>
-					<td><a href="javascript:void(0);">SW 5</a></td>
-					<td><a href="javascript:void(0);">SW 8</a></td>
-					<td><a href="javascript:void(0);">SW 9</a></td>
-					<td><a href="javascript:void(0);"></a></td>
+					<td><a href="eachbuildSW1.php">SW 1</a></td>
+					<td><a href="eachbuildSW2.php">SW 2</a></td>
+					<td><a href="eachbuildSW3.php">SW 3</a></td>
+					<td><a href="eachbuildSW5.php">SW 5</a></td>
+					<td><a href="eachbuildSW8.php">SW 8</a></td>
+					<td><a href="eachbuildSW9.php">SW 9</a></td>
 				</tr>
 			</table>
 		</div>
@@ -109,24 +106,24 @@ include("ProtoNavLogged.php");
 			<table>
 				<tr><td colspan="8">Buildings in this sector:</td></tr>
 				<tr>
-					<td><a href="javascript:void(0);">SE 1</a></td>
-					<td><a href="javascript:void(0);">SE 2</a></td>
-					<td><a href="javascript:void(0);">SE 4</a></td>
-					<td><a href="javascript:void(0);">SE 6</a></td>
-					<td><a href="javascript:void(0);">SE 8</a></td>
-					<td><a href="javascript:void(0);">SE 9</a></td>
-					<td><a href="javascript:void(0);">SE 10</a></td>
-					<td><a href="javascript:void(0);">SE 12</a></td>
+					<td><a href="eachbuildSE1.php">SE 1</a></td>
+					<td><a href="eachbuildSE2.php">SE 2</a></td>
+					<td><a href="eachbuildSE4.php">SE 4</a></td>
+					<td><a href="eachbuildSE6.php">SE 6</a></td>
+					<td><a href="eachbuildSE8.php">SE 8</a></td>
+					<td><a href="eachbuildSE9.php">SE 9</a></td>
+					<td><a href="eachbuildSE10.php">SE 10</a></td>
+					<td><a href="eachbuildSE12.php">SE 12</a></td>
 				</tr>
 				<tr>
-					<td><a href="javascript:void(0);">SE 14</a></td>
-					<td><a href="javascript:void(0);">SE 16</a></td>
-					<td><a href="javascript:void(0);">SE 19</a></td>
-					<td><a href="javascript:void(0);">SE 30</a></td>
-					<td><a href="javascript:void(0);">SE 40</a></td>
-					<td><a href="javascript:void(0);">SE 41</a></td>
-					<td><a href="javascript:void(0);">SE 42</a></td>
-					<td><a href="javascript:void(0);">SE 50</a></td>
+					<td><a href="eachbuildSE14.php">SE 14</a></td>
+					<td><a href="eachbuildSE16.php">SE 16</a></td>
+					<td><a href="eachbuildSE19.php">SE 19</a></td>
+					<td><a href="eachbuildSE30.php">SE 30</a></td>
+					<td><a href="eachbuildSE40.php">SE 40</a></td>
+					<td><a href="eachbuildSE41.php">SE 41</a></td>
+					<td><a href="eachbuildSE42.php">SE 42</a></td>
+					<td><a href="eachbuildSE50.php">SE 50</a></td>
 				</tr>
 			</table>
 		</div>
---
+++ Master/eachbuildSE19.php
@@ -0,0 +1,118 @@
+<?php
+require_once('PDO_conn.php');
+include("ProtoHeader.php");
+
+$haveSignedIn = $user->is_loggedin();
+
+$myConnection = $DB_conn;
+$myQuery = $myConnection->prepare(
+				"SELECT * 
+				FROM comments WHERE building = 'SE19'"
+				);
+$myQuery->execute();
+/*$result = $myQuery->setFetchMode(PDO::FETCH_ASSOC);*/
+$result = $myQuery;
+?>
+
+<!--Page-specific <title>-->
+<title>Virtual Tour - NE1</title>
+<!--Page-specific <js and css>-->
+
+<link rel="stylesheet" href="css/eachbuild.css" media="screen">
+<link rel="stylesheet" href="css/comment.css" media="screen">
+<script src="css/comment.js"></script>
+
+<?php
+include("ProtoNavLogged.php");
+?>
+
+		<div id="mainContent">  <!-- Insert your part of the website here. -->
+		<div class="actualContent"> <!--Helps contain individual members' page -->
+			<div class="buildingImages">
+		<div class="mapImage">
+			<div class="divBorders">
+				<img src="buildingpics/monitorlarge.png" alt="map image"/>
+			</div>
+		</div>
+		<div class="galleryImages galleryBox">
+			<div class="galleryImages top">
+				<div class="divBorders galleryImages">
+					<img class="galleryImages" src="buildingpics/monitorsmall1.png" alt="gallery image description"/>
+				</div>
+			</div>
+			<div class="galleryImages bottom">
+				<div class="divBorderOuter galleryImages">
+					<div class="galleryImages bottomLeft">
+						<div class="divBorders galleryImages">
+							<img class="galleryImages" src="buildingpics/monitorsmall2.png" alt="gallery image description"/>
+						</div>
+					</div>
+					<div class="galleryImages bottomRight">
+						<div class="divBorders galleryImages">
+							<img class="galleryImages" src="buildingpics/monitorsmall2.png" alt="gallery image description"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="descriptionArea">
+		<div class="divBorders">
+			<h1>SE19</h1>
+			<p>
+			
+				This site does not currently have a description or photos.<br/><br/>
+				
+				Help contribute to the site by posting links to image hosting sites such as Imgur 
+				and give us some descriptions!<br/>&nbsp;
+			</p>
+		</div>
+	</div>
+		</div> <!--End of actualContent-->
+				<div class="form_AllWrapper">
+			<div id="myForm" class="form_SubmitComments">
+				<!--<p>A<br>A<br>A<br>A<br>A<br>A<br>A</p>-->
+				<?php
+					foreach($result as $rows) {
+				?>
+				<table class="expandingTable">
+				<tr>
+				<td class="expandingComment"><?php echo $rows['username'];?> says:</td>
+				<td class="expandingComment"><?php echo $rows['usercomment'];?></td>
+				<!--<td>&nbsp;&nbsp;&nbsp;</td>-->
+				<td class="expandingComment alignRight"><?php echo $rows['post_date'];?></td>
+				</tr>
+				</table>
+				<br>
+				<?php
+					}
+				?>
+			</div>
+			<div id="formWrap" class="form_Wrapper">
+				<form id="form_CommentsForm" action="formSubmit.php" onsubmit="return form_validate()" method="post">
+					<div class="form_TextSection"><input type="hidden" name="buildingID" value="SE19">
+						<textarea id="form_TextArea" name="form_TextComment" rows="4" placeholder="Enter Comments"></textarea><input id="form_SubmitButton" type="submit" name="form_SubmitForm">
+					</div>
+				</form>
+			</div>
+		</div>
+		</div>  <!--End of mainContent-->
+
+	
+<script>
+var php_var = "<?php echo $haveSignedIn; ?>";
+if(php_var){
+	//document.getElementById("In").className = "signIn";
+	//document.getElementById("Out").className = "";
+	document.getElementById("form_SubmitButton").className = ""
+	document.getElementById("form_CommentsForm").className = "";
+	document.getElementById("formWrap").className = "form_Wrapper";
+} else {
+	//document.getElementById("In").className = "";
+	//document.getElementById("Out").className = "signOut";
+	document.getElementById("form_SubmitButton").className = "signOut"
+	document.getElementById("form_CommentsForm").className = "signOut";
+	document.getElementById("formWrap").className = "signOut";
+}
+</script>
+<?php include ("ProtoFooter.php"); ?>
