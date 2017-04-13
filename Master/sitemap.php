9e65bbbaad78f988d2cb9e8eb7f1c5713af7d34b
Akemi <akharaguchi@gmail.com>
2017-04-13 11:50:07 -0700

    Upped finished sitemap.

---
+++ Master/sitemap.php
@@ -0,0 +1,31 @@
+<?php include("ProtoHeader.php"); ?>
+<!--Page-specific <title>-->
+<title>YOUR MOTHER</title>
+<!--Page-specific <js and css>-->
+<?php include("ProtoNavLogged.php"); ?>
+
+	<div id="mainContent">  <!-- Insert your part of the website here. -->
+			<h2 id="mapTitle">Virtual Tour - Site Map<h1>
+			<h3 class="mapHeading"><a href="index.php">Home<br></a></h3>
+			<h3 class="mapHeading"><a href="allbuild.php">Buildings</a></h3>
+			<h4 class="mapCategory"><a href ="eachbuildNE1.php">Northeast 1 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE1.php">Southeast 1 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE2.php">Southeast 2 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE2.php">Southeast 6 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE12.php">Southeast 12 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE14.php">Southeast 14 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSE16.php">Southeast 16 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSW1.php">Southwest 1 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSW3.php">Southwest 3 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSW5.php">Southwest 5 Building</a></h4>
+			<h4 class="mapCategory"><a href ="eachbuildSW9.php">Southwest 9 Building</a></h4>
+			<h4 class="mapCategory"><a href ="buildConst.php">Other Buildings<br></a></h4>
+			<h3 class="mapHeading"><a href="glossary.php">Glossary</a></h3>
+			<h3 class="mapHeading"><a href="contactus.php">Contact Us</a></h3>
+			<h3 class="mapHeading"><a href="registration.php">Register</a></h3>
+			<h3 class="mapHeading"><a href="login.php">Log In</a></h3>
+				</div> <!--end of content-->
+			</div> <!--end of bodyarea-->
+		</div>  <!--End of mainContent-->
+
+<?php include ("ProtoFooter.php"); ?>
