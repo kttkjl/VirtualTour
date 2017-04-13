e1e87be60917da6e4a948deaad5d021c36148791
Ian Lo <ilo5@my.bcit.ca>
2017-04-13 12:32:14 -0700

    removed 'your mother' from the title of the sitemap page

--- Master/sitemap.php
+++ Master/sitemap.php
@@ -1,7 +1,8 @@
 <?php include("ProtoHeader.php"); ?>
 <!--Page-specific <title>-->
-<title>YOUR MOTHER</title>
+<title>Virtual Tour SiteMap</title>
 <!--Page-specific <js and css>-->
+<link rel="stylesheet" href="css/sitemap.css" media="screen">
 <?php include("ProtoNavLogged.php"); ?>
 
 	<div id="mainContent">  <!-- Insert your part of the website here. -->
